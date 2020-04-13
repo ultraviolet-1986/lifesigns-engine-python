4f4cfe2f6adadf234b277f0a7b36ed2b9a78f216
William Whinn <w.willis.1986@gmail.com>
2020-04-13 22:20:18 +0100

    Update lifesigns_engine.py

--- lifesigns_engine.py
+++ lifesigns_engine.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 
+"""This script will connect to others. Import for use in-game."""
+
 ##########
 # Notice #
 ##########
 
-"""
-Lifesigns Engine: A Python-based text adventure game engine.
-Copyright (C) 2019 William Willis Whinn
+# Lifesigns Engine: A Python-based text adventure game engine.
+# Copyright (C) 2020 William Willis Whinn
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ##############
 # Directives #
@@ -60,7 +60,7 @@ LIFESIGNS_METADATA_URL = 'https://github.com/ultraviolet-1986/lifesigns-engine-p
 
 def display_prompt():
     """ Display the main Lifesigns Engine header """
-    print("Lifesigns Engine v{0} Copyright (C) 2019 William Whinn\n{1}\n".format(
+    print("Lifesigns Engine v{0} Copyright (C) 2020 William Whinn\n{1}\n".format(
         LIFESIGNS_METADATA_VERSION, LIFESIGNS_METADATA_URL))
 
 #############
