987f7b76bddcfcb42b3b60ffb949cf8e0fb5f39d
William Whinn <w.willis.1986@gmail.com>
2020-06-17 18:54:58 +0100

    Update __init__.py scripts

--- __init__.py
+++ __init__.py
@@ -1,12 +1,34 @@
-###########################
-# Display Engine Metadata #
-###########################
+#!/usr/bin/env python3
+
+##########
+# Notice #
+##########
+
+# Lifesigns Engine: A Python-based text adventure game engine.
+# Copyright (C) 2020 William Willis Whinn
+
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
+###########
+# Imports #
+###########
+
+# Lifesigns Engine metadata and main header.
 
 from .lifesigns_engine import display_prompt
 
-display_prompt()
-
-# Import game objects to top-level.
+# Lifesigns Engine assets.
 
 from .assets import EMail
 from .assets import Firearm
@@ -15,3 +37,11 @@ from .assets import Journal
 from .assets import MetalKey
 from .assets import ProgrammableKeycard
 from .assets import StickyNote
+
+#############
+# Kickstart #
+#############
+
+display_prompt()
+
+# End of File.
