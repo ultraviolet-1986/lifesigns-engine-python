939e7ab328d08e8870ff3e17676ba4d339dbd559
William Whinn <w.willis.1986@gmail.com>
2020-06-17 18:41:49 +0100

    Describe the module, update license notice

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .assets import EMail
+from .assets import Firearm
+from .assets import Inventory
+from .assets import Journal
+from .assets import MetalKey
+from .assets import ProgrammableKeycard
+from .assets import StickyNote
---
+++ assets/__init__.py
@@ -0,0 +1,9 @@
+# Import game objects
+
+from .email import EMail
+from .firearm import Firearm
+from .inventory import Inventory
+from .journal import Journal
+from .metal_key import MetalKey
+from .programmable_keycard import ProgrammableKeycard
+from .sticky_note import StickyNote
